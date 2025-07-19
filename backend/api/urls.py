3dda358a9a18c1912e49dd962288af4c11b62140
Anastasiia Basyuk <basyuk1995@gmail.com>
2025-07-19 21:23:07 +0300

    urls draft + auth settings

---
+++ backend/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+# from .views import (CustomUsersViewSet, IngredientViewSet,
+#                     RecipeViewSet, TagViewSet)
+
+
+app_name = 'api'
+
+router = DefaultRouter()
+# router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+# router.register(r'recipes', RecipeViewSet, basename='recipes')
+# router.register(r'tags', TagViewSet, basename='tags')
+# router.register(r'users', CustomUsersViewSet, basename='users')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth', include('djoser.urls.authtoken')),
+]
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from api import constants as con
@@ -28,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
     'recipes.apps.RecipesConfig'
@@ -112,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -125,4 +132,18 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'HIDE_USERS': False,
+    'PERMISSIONS': {
+        'user_list': ['rest_framework.permissions.AllowAny'],
+        # IsAuthenticatedOrReadOnly ?
+        'user': ['rest_framework.permissions.IsAuthenticated'],
+    },
 }
