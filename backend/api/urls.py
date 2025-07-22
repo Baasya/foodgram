27219038b2217af1c0cada5a9a913eb9965dcf23
Anastasiia Basyuk <basyuk1995@gmail.com>
2025-07-22 22:26:46 +0300

    Simple viewsets created

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,17 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-# from .views import (CustomUsersViewSet, IngredientViewSet,
-#                     RecipeViewSet, TagViewSet)
+from .views import (CustomUserViewSet, IngredientViewSet,
+                    RecipeViewSet, TagViewSet)
 
 
 app_name = 'api'
 
 router = DefaultRouter()
-# router.register(r'ingredients', IngredientViewSet, basename='ingredients')
-# router.register(r'recipes', RecipeViewSet, basename='recipes')
-# router.register(r'tags', TagViewSet, basename='tags')
-# router.register(r'users', CustomUsersViewSet, basename='users')
+router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'users', CustomUserViewSet, basename='users')
 
 
 urlpatterns = [
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+# from django.db.models import Avg
+# from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+from djoser.views import UserViewSet
+from rest_framework import filters, serializers, status, viewsets
+from rest_framework.decorators import action, api_view
+from rest_framework import permissions
+from rest_framework.permissions import (AllowAny, IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+from rest_framework.response import Response
+
+from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                            RecipeTag, ShoppingCart, Tag)
+from users.models import CustomUser, Subscription
+
+# import constants as con
+# from .filter import 
+from .pagination import CustomPagination
+from .permissions import IsAdminOrAuthorOrReadOnly
+from .serializers import (CustomUserSerializer, IngredientSerializer,
+                          TagSerializer, RecipeReadSerializer,
+                          RecipeWriteSerializer)
+
+
+class CustomUserViewSet(UserViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = CustomPagination
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (IsAdminOrAuthorOrReadOnly,)
+    pagination_class = None
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = (AllowAny,)
+    pagination_class = None
+    filter_backends = (DjangoFilterBackend,)
+    # filterset_class = IngredientFilter
+    search_fields = ('^name',)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    # serializer_class через метод
+    permission_classes = (IsAdminOrAuthorOrReadOnly,)
+    pagination_class = CustomPagination
+    filter_backends = (DjangoFilterBackend,)
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return RecipeReadSerializer
+        return RecipeWriteSerializer
