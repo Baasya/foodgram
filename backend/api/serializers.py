11476f03215cdf783a181f02c83dd1c26b08cde6
Anastasiia Basyuk <basyuk1995@gmail.com>
2025-07-23 18:28:27 +0300

    Added serializers for users

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,18 +1,67 @@
-from djoser.serializers import UserSerializer
+from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             RecipeTag, ShoppingCart, Tag)
 from users.models import CustomUser, Subscription
 
+import constants as con
 from .fields import Base64ImageField
 
 
-class CustomUserSerializer(UserSerializer):
+class AvatarSerializer(serializers.ModelSerializer):
+    """Сериализатор для работы с аватаром."""
+    avatar = Base64ImageField(allow_null=True)
 
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ('avatar',)
+
+
+class CustomUserSerializer(UserSerializer):
+    """Сериализатор для информации о пользователях."""
+
+    is_subscribed = serializers.SerializerMethodField()
+    avatar = Base64ImageField(allow_null=True, required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'avatar',
+            'is_subscribed',
+        )
+
+        def get_is_subscribed(self, obj):
+            """Определяем значение поля is_subscribed."""
+            request = self.context.get('request')
+            if request is None or request.user.is_anonymous:
+                return False
+            return Subscription.objects.filter(
+                user=request.user,
+                author=obj).exists()
+        # request.user.follower.filter(author=obj).exists()
+
+
+class CustomUserCreateSerializer(UserCreateSerializer):
+    """Сериализатор для создания пользователя."""
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+        )
 
 
 class TagSerializer(serializers.ModelSerializer):
