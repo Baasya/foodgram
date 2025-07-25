ac021f7d69d8fc9b1e0531e6ccd62b929024e243
Anastasiia Basyuk <basyuk1995@gmail.com>
2025-07-25 12:37:49 +0300

    Added FilterSet for Recipe Model

--- backend/api/filter.py
+++ backend/api/filter.py
@@ -1,9 +1,10 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag, Recipe
 
 
 class IngredientFilter(FilterSet):
+    """Класс фильтрации по названию ингредиентов."""
     name = filters.CharFilter(
         field_name='name',
         lookup_expr='istartwith'
@@ -12,3 +13,18 @@ class IngredientFilter(FilterSet):
     class Meta:
         model = Ingredient
         fields = ('name',)
+
+
+class RecipeFilter(FilterSet):
+    """Класс фильтрации для представления рецептов."""
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
+    is_favorited = filters.BooleanFilter()
+    is_in_shopping_cart = filters.BooleanFilter()
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'is_favorited', 'is_in_shopping_cart', 'tags')
