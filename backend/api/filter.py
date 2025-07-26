891a3effee9876b4a87974ce059db8c48b2f145a
Anastasiia Basyuk <basyuk1995@gmail.com>
2025-07-26 13:36:14 +0300

    Debuging Postman 10/10.

--- backend/api/filter.py
+++ backend/api/filter.py
@@ -22,9 +22,30 @@ class RecipeFilter(FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all(),
     )
-    is_favorited = filters.BooleanFilter()
-    is_in_shopping_cart = filters.BooleanFilter()
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
         fields = ('author', 'is_favorited', 'is_in_shopping_cart', 'tags')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated:
+            user = self.request.user
+        else:
+            user = None
+
+        if value and user:
+            return queryset.filter(favorite__user_id=user.id)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if self.request.user.is_authenticated:
+            user = self.request.user
+        else:
+            user = None
+        if value and user:
+            return queryset.filter(shopping_cart__user_id=user.id)
+        return queryset
