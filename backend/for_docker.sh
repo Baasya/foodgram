a79ff9764f54bcd52fcdbfdbd16010f4d03c31ed
Anastasiia Basyuk <basyuk1995@gmail.com>
2025-07-27 15:13:44 +0300

    Deploy 9. Backend Dockerfile

--- backend/for_docker.sh
+++ backend/for_docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+set -e
 
 python manage.py migrate 
 
@@ -7,3 +8,5 @@ python manage.py collectstatic
 python manage.py import_csv ./data/ingredients.csv
 
 cp -r /app/collected_static/. /backend_static/static/
+
+exec "$@"
