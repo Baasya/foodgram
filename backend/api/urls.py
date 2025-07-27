1f8a3b815eaa2057f399bdc66bdbea0585c16128
Anastasiia Basyuk <basyuk1995@gmail.com>
2025-07-27 12:34:43 +0300

    Deploy

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -15,5 +16,7 @@ router.register(r'users', views.CustomUserViewSet, basename='users')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include('djoser.urls')),
+    path('docs/', TemplateView.as_view(template_name='docs/redoc.html'),
+         name='redoc'),
     path('auth/', include('djoser.urls.authtoken')),
 ]
---
+++ backend/for_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+set -e
+
+python manage.py migrate 
+
+python manage.py collectstatic
+
+python manage.py import_csv ./data/ingredients.csv
+
+cp -r /app/collected_static/. /backend_static/static/
+
+exec "$@"
