dd785ea7f2f0833918caf4d8bd972a6e54ce0e0b
Anastasiia Basyuk <basyuk1995@gmail.com>
2025-07-30 00:50:55 +0300

    def subscribe

--- backend/api/views.py
+++ backend/api/views.py
@@ -119,8 +119,8 @@ class CustomUserViewSet(UserViewSet):
     )
     def subscribe(self, request, id):
         user = request.user
+        author = get_object_or_404(User, id=id)
         if self.request.method == 'POST':
-            author = get_object_or_404(User, id=id)
             data = {'user': user.id, 'author': author.id}
             serializer = SubscriptionSerializer(
                 data=data,
